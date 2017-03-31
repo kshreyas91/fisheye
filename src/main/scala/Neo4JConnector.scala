383842a88932be92794aa63993c642562fa11dae
Thomas (Hongjian) Yang <jscnm1993@gmail.com>
2017-03-30 22:19:00 -0400

    add init code for connector

--- src/main/scala/Neo4JConnector.scala
+++ src/main/scala/Neo4JConnector.scala
@@ -2,20 +2,54 @@
   * Created by yanghj on 3/30/17.
   */
 
+package Neo4jConnector
+
+import Neo4jAsyncDriver.Neo4jAsyncDriver
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
 sealed trait FieldType
-case object Phone_number extends FieldType
-case object Vehicle_plate extends FieldType
+case object PhoneNumber extends FieldType
+case object VehiclePlate extends FieldType
 case object Name extends FieldType
 case object Unknown extends FieldType
-case object Credit_card_number extends FieldType
-case object Ip_number
+case object CreditCardNumber extends FieldType
+case object IpNumber extends FieldType
 case object Date extends FieldType
-case object Street_addresse extends FieldType
+case object StreetAddress extends FieldType
 case object Email extends FieldType
 
 case class Concept(name: String)
 case class FieldName(field: FieldType)
 
 class Neo4JConnector {
+  val classes = Array(
+    "PhoneNumber",
+    "VehiclePlate",
+    "Name",
+    "CreditCardNumber",
+    "IpNumber",
+    "Date",
+    "StreetAddress",
+    "Email"
+  )
+  val driver = new Neo4jAsyncDriver
 
+  def drop(): Unit = {
+    driver.run("MATCH (n) DETACH DELETE n")
+  }
+
+  def init(): Unit = {
+    var r = driver.run("CREATE (n:Concept {name: \"PII\"})")
+    r onComplete { case _ =>
+      driver.run(
+        """
+          | MATCH (n:Concept {name:"PII"})
+          | FOREACH (name in {classes} |
+          | CREATE (n)-[:include]->(:Concept {name:name}))
+        """.stripMargin, Map(
+          "classes" -> classes
+        ))
+    }
+  }
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,12 @@
 /**
   * Created by yanghj on 3/30/17.
   */
-import Neo4jAsyncDriver.Neo4jAsyncDriver
+import Neo4jConnector.Neo4JConnector
 
 object main {
   def main(args: Array[String]): Unit = {
+    val driver = new Neo4JConnector
+    driver.init()
     println("Hello, world!")
-    var driver = new Neo4jAsyncDriver
-    var res = driver.run("CREATE (a:Person {name: {name}, title: {title}})", Map(("name", "lalala"), ("title", "hehe")))
-    res = driver.run( "MATCH (a:Person) WHERE a.name = {name} RETURN a.name AS name, a.title AS title", Map(("name", "lalala")))
-    println(res)
   }
 }
